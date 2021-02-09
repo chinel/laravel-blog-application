de633a0982a98c7a33de9a022fcb0749d48b6f2d
nelo <nelokwaz@gmail.com>
2021-02-10 00:12:10 +0100

    created a blogs table seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -27,24 +27,6 @@ class UsersTableSeeder extends Seeder
                 'updated_at' => \Carbon\Carbon::now()
             ),
 
-
-
-        ));  DB::table('users')->insert(array(
-
-        array(
-            'firstname' => 'Top',
-            'lastname' => 'Admin',
-            'email' => 'admin@blog.com',
-            'role_id' => 1,
-            'active' => 1,
-            'password' => bcrypt('ADMIN12345'),
-            'remember_token' => null,
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now()
-        ),
-
-
-
     ));
     }
 }
