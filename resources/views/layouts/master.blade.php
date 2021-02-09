8ed25202354388f00537cc104564d9a79006f5ad
nelo <nelokwaz@gmail.com>
2021-02-09 20:01:24 +0100

    made some corrections

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+
 
     <title>@yield('page-title') - Blog.com</title>
 
